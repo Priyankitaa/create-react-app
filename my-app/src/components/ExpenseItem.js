54af8aa00a769456c50c7aeba5ce1f3b12a33711
Priyankitaa <riyasaxena1998@gmail.com>
2023-12-04 16:25:07 +0530

    Task 1.8

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.57;
+    const locationOfExpenditure = 'Car'
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.57</div>
+        <h2>{expenseTitle}</h2>
+        <div>LocationOfExpenditure: {locationOfExpenditure}</div>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
